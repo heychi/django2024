abf613df5ce5a02b06386008bb9644831fc588cc
heychi <heychi0728@gmail.com>
2024-10-05 22:26:25 +0900

    feat: 로그인 및 회원가입 구현

--- my_project/settings.py
+++ my_project/settings.py
@@ -28,6 +28,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'product',
     'pages',
+    'accounts.apps.AccountsConfig',
+
 ]
 
 MIDDLEWARE = [
@@ -116,3 +118,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
